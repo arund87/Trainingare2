69cf8c151a3c48987ea13a8bdf9132df997c650a
arund87 <arund87@gmail.com>
2016-12-23 12:09:49 +0530

    New project

---
+++ src/test/java/testpack1/Testclass.java
@@ -0,0 +1,25 @@
+package testpack1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Testclass {
+     
+	WebDriver driver;
+        @Test
+	public void wordpressrun() {
+	
+      driver =new FirefoxDriver();
+      driver.get("https://wordpress.com/");
+      driver.manage().window().maximize();
+      driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+      driver.findElement(By.xpath("//a[@title='Get Started']")).click();
+      driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+      driver.quit();
+	}
+
+}
